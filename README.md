fb6834b44fde7011c128891806c88e4e8cd47bbe
Wiktor Flis <wikflis@gmail.com>
2022-05-06 22:31:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Notebooks with attempts on text classification
+
+[![Gradient](https://assets.paperspace.io/img/gradient-badge.svg)](https://console.paperspace.com/github/huggingface/nlp/blob/master/notebooks/Overview.ipynb)
---
+++ prepare_dataset.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+from sklearn import model_selection
+
+try:
+    print("Trying to read data from csv file...")
+    texts = pd.read_csv("data/CNN_Articels_clean.csv")
+except Exception as e:
+    print("Data not found. Downloading data from kaggle...")
+    import kaggle
+    kaggle.api.authenticate()
+    kaggle.api.dataset_download_files('hadasu92/cnn-articles-after-basic-cleaning', path='data', unzip=True)
+    print("Trying to read data from csv file...")
+    texts = pd.read_csv("data/CNN_Articels_clean/CNN_Articels_clean.csv")
+
+print("Data read successfully. Splitting data into train and validation sets...")
+
+X = texts["Description"]
+y = texts["Category"]
+
+X_train, X_validation, y_train, y_validation = model_selection.train_test_split(X, y, test_size=0.2, random_state=42)
+
+# validation set
+df = pd.DataFrame({"Description": X_validation, "Category": y_validation})
+df.to_csv("data/validation_set.csv")
+
+# training set
+df = pd.DataFrame({"Description": X_train, "Category": y_train})
+df.to_csv("data/training_set.csv")
+
+print("All done!")
